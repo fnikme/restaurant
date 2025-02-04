efa6b542ee7e5f009c0608e911154abfea2023ff
fnikme <f.n@ik.me>
2025-02-04 12:42:55 +0100

    2erreurPostman

--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Restaurant;
 use App\Repository\RestaurantRepository;
+use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,13 +15,12 @@ class RestaurantController extends AbstractController
 {
     public function __construct(private EntityManagerInterface $manager, private RestaurantRepository $repository) {}
 
-
     #[Route(name: 'new', methods: ['GET', 'POST'])]
     public function new(): Response
     {
         $restaurant = new Restaurant();
-        $restaurant->setName(name: 'Quai Antique');
-        $restaurant->setDescription(description: 'Cette qualité et ce goût par le chef Arnaud MICHANT.');
+        $restaurant->setName('Quai Antique');
+        $restaurant->setDescription('Cette qualité et ce goût par le chef Arnaud MICHANT.');
         $restaurant->setCreatedAt(new \DateTimeImmutable());
 
         $this->manager->persist($restaurant);
@@ -28,7 +28,7 @@ class RestaurantController extends AbstractController
 
         return $this->json(
             ['message' => "Restaurant resource created with {$restaurant->getId()} id"],
-            status: Response::HTTP_CREATED,
+            Response::HTTP_CREATED,
         );
     }
 
@@ -37,7 +37,7 @@ class RestaurantController extends AbstractController
     {
         // $restaurant CHERCHER RESTAURANT ID = 1
         if (!$restaurant) {
-            throw new \Exception(message: "No Restaurant fount for {$id} id");
+            throw $this->createAccessDeniedException(message: "No Restaurant fount for {$id} id");
         }
 
         return $this->json(
@@ -50,7 +50,7 @@ class RestaurantController extends AbstractController
     {
         // $restaurant = CHERCHER RESTAURANT ID = 1
         if (!$restaurant) {
-            throw new \Exception(message: "No Restaurant fount for {$id} id");
+            throw $this->createAccessDeniedException(message: "No Restaurant fount for {$id} id");
         }
 
         $restaurant->setName('Restaurant name update');
@@ -64,8 +64,11 @@ class RestaurantController extends AbstractController
     {
         // $restaurant = CHERCHER RESTAURANT ID =
         if (!$restaurant) {
-            throw new \Exception(message: "No Restaurant fount for {$id} id");
+            throw $this->createAccessDeniedException(message: "No Restaurant fount for {$id} id");
         }
+
+        $this->manager->remove($restaurant);
+        $this->manager->flush();
 
         return $this->json(['message' => 'Restaurant ressource deleded'], status: Response::HTTP_NO_CONTENT);
     }
