1f4020cdf46e7526f86f7d6061f1aa056800ddd6
fnikme <f.n@ik.me>
2025-02-18 08:49:19 +0100

    erreurfixturesPartagées

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use DateTimeImmutable;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Exception;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(private UserPasswordHasherInterface $passwordHasher)
+    {
+    }
+
+    /** @throws Exception */
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 1; $i <= 20; $i++) {
+            $user = (new User())
+                ->setEmail("email.$i@studi.fr")
+                ->setCreatedAt(new DateTimeImmutable());
+
+            $user->setPassword($this->passwordHasher->hashPassword($user, 'password' . $i));
+
+            $manager->persist($user);
+        }
+        $manager->flush();
+    }
+}
